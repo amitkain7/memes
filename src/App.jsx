d54bf58613a42ff83556e990a5a6b7e3f63b9e69
Amit <121176159+amitkain7@users.noreply.github.com>
2023-11-08 09:53:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/header/Navbar'
 import Mainsection from './Components/main/Mainsection'
+import './App.css
 const App = () => {
   return (
     <>
